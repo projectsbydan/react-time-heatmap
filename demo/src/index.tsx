6d3872ecd2e6224f37ec0519a8c87dbc050cdb7d
Dan Deaconu <36627723+ltndan@users.noreply.github.com>
2020-06-17 22:12:26 +0200

    added demo to the project

---
+++ demo/src/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import { ITimeEntry, TimeHeatMap } from "react-time-heatmap";
+import { getTimeEntries } from "./example";
+
+const App = () => {
+  const [entries, setEntries] = useState<ITimeEntry[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      const data = getTimeEntries();
+      setEntries(data);
+    }, 1000);
+  }, []);
+
+  return (
+    <TimeHeatMap
+      timeEntries={entries}
+      showCounts={false}
+      numberOfGroups={4}
+      textForNoTimeEntries="No time entries"
+    />
+  );
+};
+
+const container = document.getElementById("root");
+render(<App />, container);
